648f28fae5a1266d249935ca61ecb4a6a796b50b
GuiOP11 <deoliveirapadilhag@gmail.com>
2026-08-05 22:04:21 -0300

    commit2

--- UNOESC/Seg2026/Atividades/Palavras/Program.cs
+++ UNOESC/Seg2026/Atividades/Palavras/Program.cs
@@ -1,8 +1,22 @@
 ﻿string firstName = "Bilbo";
 string lastName = "Bolseiro";
 
-string note = lastName.ToUpper();
-        +"" + firstName;
+string note = lastName.ToUpper()
+        + "" + firstName;
 
-string initials = lastName[0] + firstName[0];
+string initials = lastName[0] + " "
+                +   firstName[0];
+
+Console.WriteLine ("" + initials);
+
+// formatation to string
+
+string texto = string.Format (
+    "{0} {1} nascido em {2}",
+    firstName,
+    lastName,
+    "1984"
+);
+
+Console.WriteLine (texto);
 
