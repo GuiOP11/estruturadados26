142cd0a9011265d7f138f18ebf4af84ec59cd497
GuiOP11 <deoliveirapadilhag@gmail.com>
2026-08-05 21:40:23 -0300

    filhadaputa

--- UNOESC/Seg2026/Atividades/Palavras/Program.cs
+++ UNOESC/Seg2026/Atividades/Palavras/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string firstName = "Bilbo";
+string lastName = "Bolseiro";
+
+string note = lastName.ToUpper();
+        +"" + firstName;
+
+string initials = lastName[0] + firstName[0];
+
