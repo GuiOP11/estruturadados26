d049563654eb20d5d20d21c877f07d4a551c99b6
GuiOP11 <deoliveirapadilhag@gmail.com>
2026-08-12 20:22:22 -0300

    vetores12

--- UNOESC/Seg2026/Atividades/Vetores/Vetores/Program.cs
+++ UNOESC/Seg2026/Atividades/Vetores/Vetores/Program.cs
@@ -39,3 +39,66 @@ string middleName = "Padilha";
 // Interpolção de string
 string message = $"Nome completo: {firstName} {middleName} {lastName}";
 Console.WriteLine (message);
+
+
+/* Vetores
+Vetores UNI-DIMENSIONAIS
+Sintaxe: type[]
+*/
+
+//Declaração sem inicialização
+//Obs: Variável alocada mas nula
+
+int[] numbers;
+
+//inicializando o vetor
+numbers = new int[5];
+
+// Atribuindo calores ao vetor
+number[0] = 10;
+number[1] = 20;
+number[2] = 30;
+number[3] = 40;
+number[4] = 50;
+
+//É possivel declarar o vetor e já definir e atribuir
+
+int[]  number2 =
+            new int[] {100, 200, 300};
+    
+int[] number3 =
+             {1000, 2000, 3000,4000};
+// Percorrendo um vetor e adicionando valores dinamicamente
+
+Console.WriteLine("Iniciando com vetores"
+);
+
+Console.WriteLine("Informe o tamanho do vetor de números de inteiros: ")
+int size =  Convert.ToInt32 (
+    Console.ReadLine()
+    );
+
+int[] myArray = new int[size];
+int total = 0; //Acumuladora
+int counter = 0;
+                    //flag
+for(int i = 0; i < myArray.Length; i++)
+{
+    Console.WriteLine(
+        "Digite para [" +i+ "]: "
+    );
+    myArray[i] = Convert.ToInt32(
+        Console.ReadLine()
+    );
+
+    total += myArray[i];
+    couter++;
+
+}
+Console.WriteLine(
+        "Contagem = "+ counter
+        );
+
+
+
+
